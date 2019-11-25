33fd7d68d77c1d4043c5b28d65446af4a0cabdb2
Neeraj Sharma <neerajsharma9195@gmail.com>
2019-11-25 13:00:09 -0500

    Fix: logistic regression with standard sklearn logistic

--- src/classifiers/logistic.py
+++ src/classifiers/logistic.py
@@ -2,10 +2,11 @@ import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 from src.sketches.count_sketch import CountSketch
 from src.processing.parse_data import process_data_to_numpy
+from sklearn.linear_model import LogisticRegression
 import os
 
 
-class LogisticRegression(object):
+class OurLogisticRegression(object):
     def __init__(self, num_features):
         self.D = num_features
         self.w = np.array([0] * self.D)
@@ -66,6 +67,23 @@ class LogisticRegression(object):
         print("grads dict {}".format(grads))
 
 
+def standard_logistic_regression(features, labels, D):
+    X = np.zeros(shape=(1000, D))
+    for i in range(1000):
+        print("i {}".format(i))
+        current_example = features[i]
+        for feature_pos, feature_val in current_example:
+            print("feature pos {} feature val {}".format(feature_pos, feature_val))
+            X[i][feature_pos - 1] = feature_val
+        #logistic.train(example, labels[i])
+    logistic_reg = LogisticRegression()
+    logistic_reg.fit(X, labels[0:1000])
+    print("intercept for standard model {}".format(logistic_reg.intercept_))
+    pred_labels = (logistic_reg.predict(X))
+    accuracy = sum(pred_labels == labels[0:1000])/1000
+    print(accuracy)
+
+
 if __name__ == '__main__':
     # X = np.load('/Users/neerajsharma/my_work/umass/umass_study/1st_sem/CS689/final_project/src/data/q2_train_X.npy')
     # y = np.load('/Users/neerajsharma/my_work/umass/umass_study/1st_sem/CS689/final_project/src/data/q2_train_y.npy')
@@ -79,26 +97,29 @@ if __name__ == '__main__':
     filePath = os.path.join(data_directory_path, fileName)
     labels, features = process_data_to_numpy(filePath)
     D = 47236
-    logistic = LogisticRegression(D)
-    for i in range(len(labels)):
-        print("i {}".format(i))
-        current_example = features[i]
-        example = np.array([0]*D)
-        for feature_pos, feature_val in current_example:
-            print("feature pos {} feature val {}".format(feature_pos, feature_val))
-            example[feature_pos-1] = feature_val
-        logistic.train(example, labels[i])
+    standard_logistic_regression(features, labels, D)
 
-    pred_labels = []
-    for i in range(len(labels)):
-        print("i {}".format(i))
-        current_example = features[i]
-        example = np.array([0] * D)
-        for feature_pos, feature_val in current_example:
-            print("feature pos {} feature val {}".format(feature_pos, feature_val))
-            example[feature_pos - 1] = feature_val
-        pred_labels.append(logistic.predict(example))
-    print(sum(pred_labels == labels) / len(labels))
+    # logistic = OurLogisticRegression(D)
+    # for i in range(850):
+    #     print("i {}".format(i))
+    #     current_example = features[i]
+    #     example = np.array([0] * D)
+    #     for feature_pos, feature_val in current_example:
+    #         print("feature pos {} feature val {}".format(feature_pos, feature_val))
+    #         example[feature_pos - 1] = feature_val
+    #     logistic.train(example, labels[i])
+    #
+    # pred_labels = []
+    # for i in range(850):
+    #     print("i {}".format(i))
+    #     current_example = features[i]
+    #     example = np.array([0] * D)
+    #     for feature_pos, feature_val in current_example:
+    #         example[feature_pos - 1] = feature_val
+    #     pred_labels.append(logistic.predict(example))
+    # print(sum(pred_labels == labels[0:850]) / 850)
+    # with open("pred_label_from_logistic_reg.txt", 'w') as f:
+    #     f.write(pred_labels)
     # n_t, d_t = X_test.shape
     # output = [logistic.predict(X_test[i]) for i in range(n_t)]
     # print(sum(output == y_test) / n_t)
