cb21836bca154b41ff940ac8baf98806dbd6f723
Neeraj Sharma <neerajsharma9195@gmail.com>
2020-03-08 10:14:58 -0400

    Fix: added a generic flow for all sketches logisitic classifier

--- src/independent_study/logistic_regression/logistic_regression_complementary_count_min.py
+++ src/independent_study/logistic_regression/logistic_regression_complementary_count_min.py
@@ -1,5 +1,8 @@
 import numpy as np
 from src.sketches.custom_count_min_sketch import CustomCountMinSketch
+from src.sketches.conservative_count_min_sketch import ConservativeCountMinSketch
+from src.sketches.count_sketch import CountSketch
+from src.sketches.custom_count_sketch import CustomCountSketch
 from src.processing.parse_data import process_data
 import os
 import math
@@ -10,9 +13,16 @@ import json
 
 
 class LogisticRegression(object):
-    def __init__(self, count_sketch_size, top_k, top_k_dict={}):
+    cms_dicts = {
+        "complementary_cms": CustomCountMinSketch,
+        "complementary_cms_conservative": ConservativeCountMinSketch,
+        "mission_count_sketch": CountSketch,
+        "conservative_count_sketch": CustomCountSketch
+    }
+
+    def __init__(self, cms_type, hash_func_counts, count_sketch_size, top_k, top_k_dict={}):
         self.learning_rate = 0.5
-        self.cms = CustomCountMinSketch(2, count_sketch_size)
+        self.cms = self.cms_dicts[cms_type](hash_func_counts, count_sketch_size)
         self.top_k = TopK(top_k)
         self.top_k_dict = top_k_dict
 
@@ -32,9 +42,8 @@ class LogisticRegression(object):
             val = self.top_k.get_value_for_key(feature_pos[i]) * features[i]
             logit += val
         sigm_val = self.sigmoid(logit)
-        print("label {} sigmoid {}".format(label, sigm_val))
         loss = self.loss(y=label, p=sigm_val)
-        diff_label = (label - sigm_val) # difference in label
+        diff_label = (label - sigm_val)  # difference in label
         if diff_label != 0:
             for i in range(len(feature_pos)):
                 # updating the change only on previous values
@@ -58,7 +67,7 @@ class LogisticRegression(object):
 
 if __name__ == '__main__':
     current_directory = (os.path.dirname(__file__))
-    data_directory_path = os.path.join(current_directory, '..', 'data')
+    data_directory_path = os.path.join(current_directory, '../../', 'data')
     fileName = "rcv1_train.binary"
     filePath = os.path.join(data_directory_path, fileName)
     labels, features = process_data(filePath)
@@ -67,16 +76,25 @@ if __name__ == '__main__':
     test_filePath = os.path.join(data_directory_path, test_fileName)
     test_labels, test_features = process_data(test_filePath)
     print("test labels size {}".format(len(test_labels)))
-    count_sketch_size = 12000
     print("len of labels {}".format(len(labels)))
     correctly_classified_examples = []
     D = 47236
     time_taken = []
-    #top_k_file_path = "/Users/neerajsharma/my_work/umass/umass_study/1st_sem/CS689/project_repo/src/independent_study/topk_features_2000.txt"
-    #top_k_positions = get_top_k_positions(top_k_file_path)
+    # top_k_file_path = "/Users/neerajsharma/my_work/umass/umass_study/1st_sem/CS689/project_repo/src/independent_study/topk_features_2000.txt"
+    # top_k_positions = get_top_k_positions(top_k_file_path)
     top_k_dict = {k: [] for k in range(D)}
-    lgr = LogisticRegression(count_sketch_size=count_sketch_size, top_k=10000, top_k_dict=top_k_dict)
+    # model params
+    top_k_size = 8000
+    cms_type = "complementary_cms_conservative"
+    num_hashes = 2
+    count_sketch_size = 11000
+    lgr = LogisticRegression(cms_type=cms_type,
+                             hash_func_counts=num_hashes,
+                             count_sketch_size=count_sketch_size,
+                             top_k=top_k_size,
+                             top_k_dict=top_k_dict)
     start_time = time.time()
+    # training
     for epoch in range(0, 1):
         print("epoch {}".format(epoch))
         for i in range(len(labels)):
@@ -88,8 +106,12 @@ if __name__ == '__main__':
             feature_vals = [item[1] for item in example_features]
             loss = lgr.train_with_sketch(feature_pos, feature_vals, label)
             print("loss {}".format(loss))
-    with open("../results/topk_feature_gradients_complementary_count_min_all_topk_8000.json", 'w') as f:
-         f.write(json.dumps(lgr.top_k_dict))
+    results_dir_path = os.path.join(current_directory, '../', 'results')
+    topk_dict_filePath = os.path.join(results_dir_path,
+                                      "topk_feature_gradients_{}_all_topk_{}.json".format(cms_type,
+                                                                                          top_k_size))
+    with open(topk_dict_filePath, 'w') as f:
+        f.write(json.dumps(lgr.top_k_dict))
     end_time = time.time()
     correct = 0
     for i in range(len(test_labels)):
@@ -104,9 +126,9 @@ if __name__ == '__main__':
     print("correctly classified test examples {}".format(correct))
     correctly_classified_examples.append(correct)
     print("correct examples {}".format(correctly_classified_examples))
-    # with open(
-    #         'results/topk_features_8000.txt', 'w') as f:
-    #     for item in lgr.top_k.heap:
-    #         key = lgr.top_k.keys[item.value]
-    #         value = lgr.top_k.features[key]
-    #         f.write("{}:{}\n".format(key, value))
+    top_k_features_path = os.path.join(results_dir_path, "topk_features_{}_{}.txt".format(cms_type, top_k_size))
+    with open(top_k_features_path, 'w') as f:
+        for item in lgr.top_k.heap:
+            key = lgr.top_k.keys[item.value]
+            value = lgr.top_k.features[key]
+            f.write("{}:{}\n".format(key, value))
