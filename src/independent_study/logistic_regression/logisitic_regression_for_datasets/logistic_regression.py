Fix: stochastic gradient descent

src/independent_study/logistic_regression/logisitic_regression_for_datasets/logistic_regression.py
@@ -78,16 +78,17 @@ class LogisticRegression(object):
         print("Dataset Processing Done")
         return test_labels, test_features
 
-    def train_dataset(self, epochs):
+    def train_dataset(self, epochs, iterations):
         train_labels, train_features = self.processing_dataset_train()
         print("Dataset Training Started")
         for epoch in range(epochs):
             print("epoch {}".format(epoch))
-            for i in range(len(train_labels)):
-                print("i {}".format(i))
-                label = train_labels[i]
+            for iteration in range(iterations):
+                print("i {}".format(iteration))
+                random_index = np.random.randint(len(train_labels))
+                label = train_labels[random_index]
                 label = (1 + label) / 2
-                example_features = train_features[i]
+                example_features = train_features[random_index]
                 feature_pos = [item[0] for item in example_features]
                 feature_vals = [item[1] for item in example_features]
                 loss = self.train(feature_pos, feature_vals, label)
@@ -127,7 +128,7 @@ class LogisticRegression(object):
 
 if __name__ == '__main__':
     lgr = LogisticRegression(47326, "rcv1_train.binary", "rcv1_test.binary")
-    lgr.train_dataset(1)
+    lgr.train_dataset(1, 20000)
     lgr.accuracy_on_test()
     # lgr.dump_top_K('../../dumps/normal_logistic/topk_logistic_regression')
     # lgr.dump_gradient_updates("../../dumps/normal_logistic/logistic_regression_gradient_updates")

src/independent_study/logistic_regression/logisitic_regression_for_datasets/logistic_regression_for_sketches.py
@@ -81,8 +81,6 @@ if __name__ == '__main__':
     D = 47236
     time_taken = []
     top_k_size = 8000
-    top_k_file_path = "topk_features_{}.txt".format(top_k_size)
-    top_k_positions = get_top_k_positions(top_k_file_path)
     top_k_dict = {k: [] for k in range(D)}
     # model params
     cms_type = "complementary_cms"
@@ -95,18 +93,20 @@ if __name__ == '__main__':
                              top_k_dict=top_k_dict)
     start_time = time.time()
     # training
+    num_iterations = 20000
     for epoch in range(0, 1):
         print("epoch {}".format(epoch))
-        for i in range(len(labels)):
-            print("i {}".format(i))
-            label = labels[i]
+        for iteration in range(num_iterations):
+            print("i {}".format(iteration))
+            random_index = np.random.randint(len(labels))
+            label = labels[random_index]
             label = (1 + label) / 2
-            example_features = features[i]
+            example_features = features[random_index]
             feature_pos = [item[0] for item in example_features]
             feature_vals = [item[1] for item in example_features]
             loss = lgr.train_with_sketch(feature_pos, feature_vals, label)
             print("loss {}".format(loss))
-    results_dir_path = os.path.join(current_directory, '../', 'results')
+    results_dir_path = os.path.join(current_directory, '../../', 'results')
     topk_dict_filePath = os.path.join(results_dir_path,
                                       "topk_feature_gradients_{}_all_topk_{}.json".format(cms_type,
                                                                                           top_k_size))